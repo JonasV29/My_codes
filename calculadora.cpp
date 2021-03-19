e0818896981aeb1372615a45f74ba02061d7d155
JonasV29 <jonasvitorio35@gmail.com>
2021-03-18 22:42:58 -0300

    My first commit

---
+++ calculadora.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	float n1,n2,conta;
+	char calculador;
+	printf("digite o primeiro numero: \n");
+	scanf("%f",&n1);
+	printf("digite o segundo numero: \n");
+	scanf("%f",&n2);
+	printf("\n calculador desejado: \n");
+	scanf("%s",&calculador);
+	
+	if(calculador== '+') {
+	conta=n1+n2; 
+	printf("\n a conta deu:%.2f",conta);
+}
+   else
+   if(calculador== '-') {
+   conta=n1-n2;
+   printf("\n a conta deu:%.2f",conta);
+}
+    else
+    if(calculador=='/') {
+    conta=n1/n2;
+    printf("\n a conta deu:%.2f", conta);
+	}
+	else 
+	if(calculador=='*') {
+	conta=n1*n2;
+	printf("\n a conta deu:%.2f",conta);
+	}
+	 
+	 system("Pause");
+	 return 0;
+	
+}
