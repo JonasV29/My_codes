1f3b16e7e848af128b8c271ea92f11cf41b0db4e
Jonas Vitorio <jonasvitorio35@gmail.com>
2022-01-15 18:42:06 -0300

    this code was made in java with intellij

---
+++ Primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.Objetcs.Livro;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+        Livro livro = new Livro("Rosângela", "65", "Ser ou não ser adulto");
+
+        System.out.println(livro);
+
+
+
+    }
+}
